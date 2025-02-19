c4f4de739159f5093ebaa5c3d829baf240e293dd
MaximoffMq <jose.morales6@catolica.edu.sv>
2025-02-18 18:19:42 -0600

    nuevos detalles

---
+++ L01_2022MQ650_2022DS601/Controllers/publicacionesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L01_2022MQ650_2022DS601.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class publicacionesController : ControllerBase
+    {
+    }
+}
